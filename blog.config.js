914d3c9be5edc3aa08cd09e04b06cdf2f295ea16
Ape996 <124447311+Ape996@users.noreply.github.com>
2023-12-17 21:00:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ape M', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '躺不平，卷不动的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://atom-mailbox-75f.notion.site/726b0754ec79424dbb5f2447205f64ee?v=4e6fc75272ba4fa1bea5ac0368868fdf&pvs=4', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
